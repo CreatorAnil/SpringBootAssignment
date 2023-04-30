2875026f9a473b3e97986374e41fcab46a74244e
CreatorAnil <58660128+CreatorAnil@users.noreply.github.com>
2023-05-01 00:33:51 +0530

    Delete SpringBootDemo directory

--- SpringBootDemo/src/main/java/com/gl/boot/SpringBootDemo/database/CustomerRepo.java
+++
@@ -1,8 +0,0 @@
-package com.gl.boot.SpringBootDemo.database;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.gl.boot.SpringBootDemo.entity.Customer;
-
-public interface CustomerRepo extends CrudRepository<Customer, String>{
-}
